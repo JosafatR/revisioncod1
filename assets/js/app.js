ee859a0efb7061863a64017c4f64085b1a40159b
Josafat Robledo <josafat.robledob@gmail.com>
2024-03-22 21:02:23 -0600

    ci:correcciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,25 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+// se cambian los nombres de las variables para una mejor comprensión
+const nameDate = document.querySelector('.name'); // se agrega el . a 'name'
+const blogDate = document.querySelector('.blog'); // se cambia el # por el . en 'blog'
+const locationCase = document.querySelector('.location');
+
+async function displayUser(username) { //SE agrega async a la función para usar el await
+  //SE reemplaza el textContent por nameDate.innerHTML
+  nameDate.innerHTML = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  nameDate.innerHTML = '${data.name}'; // se convierte nameDate.innerHTML
+  blogDate.innerHTML = '${data.blog}';
+  locationCase.innerHTML = '${data.location}';
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  nameDate.innerHTML = `Algo salió mal: ${err}` //Se reemplaza la variable a nameDate.innerHTML
 }
 
 displayUser('stolinski').catch(handleError);
